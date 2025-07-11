5af573e9cf55599d8d34bc95ed94106bd11222ad
Murali Elavarthi <muralis2p@gmail.com>
2025-07-11 20:40:40 +0530

    m1

---
+++ inputs.tf
@@ -0,0 +1,15 @@
+variable "server-name" {
+  default = string
+}
+
+variable "sg_id" {
+  default = string
+}
+
+variable "ami_id" {
+  default = string
+}
+
+variable "instance_type" {
+  default = string
+}
