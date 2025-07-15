b86adb19249252d3bd73789687422fcc2d89902f
Murali Elavarthi <muralis2p@gmail.com>
2025-07-15 14:26:25 +0530

    m1

--- inputs.tf
+++ inputs.tf
@@ -1,15 +1,21 @@
 variable "server_name" {
   type = string
+  default = "new_instance"
+  description = "variable is for server name"
 }
 
 variable "sg_id" {
   type = string
+  description = "variable is for security group id"
 }
 
 variable "ami_id" {
   type = string
+  description = "variable is for ami"
 }
 
 variable "instance_type" {
   type = string
+  default = "t2.micro"
+  description = "variable is for instance type"
 }
